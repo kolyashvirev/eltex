b9d963f3cf938fbc2c0a32f24459638a5d040da7
kolyashvirev <shvirev04@yandex.ru>
2024-11-18 00:30:38 +0700

    Задание 05. Добавление проверок системных вызовов. Модификация Makefile

--- Module3/05/main.c
+++ Module3/05/main.c
@@ -44,6 +44,7 @@ int main(int argc, char* argv[])
         exit(EXIT_FAILURE);
 
     case 0:
+        if(close(pipefd[0]) == -1) perror("Ошибка закрытия канала");
         for(int i = 0; i < atoi(argv[1]); ++i)
         {
             while (fileAcc) sleep(0);
@@ -67,16 +68,23 @@ int main(int argc, char* argv[])
             write(pipefd[1], &num, sizeof(num));
             sleep(0);
         }
+        if(close(pipefd[1]) == -1) perror("Ошибка закрытия канала");
         exit(EXIT_SUCCESS);
     
     default:
         {
             sleep(0);
 
+            if(close(pipefd[1]) == -1) perror("Ошибка закрытия канала");
+
             int num;
             for(int i = 0; i < atoi(argv[1]); ++i)
             {
-                kill(pid, SIGUSR1);
+                if (kill(pid, SIGUSR1))
+                {
+                    perror("Ошибка отправки сигнала");
+                    exit(EXIT_FAILURE);
+                }
 
                 FILE* file = fopen(FILENAME, "a");
                 if(file == NULL)
@@ -90,10 +98,23 @@ int main(int argc, char* argv[])
 
                 fclose(file);
 
-                kill(pid,SIGUSR2);
+                if (kill(pid, SIGUSR2))
+                {
+                    perror("Ошибка отправки сигнала");
+                    exit(EXIT_FAILURE);
+                }
                 sleep(0);
             }
-            wait(NULL);
+            if(close(pipefd[0]) == -1) perror("Ошибка закрытия канала");
+            
+            int status;
+            wait(&status);
+            if(!WIFEXITED(status))
+            {
+                perror("Ошибка завершения дочернего процесса");
+                exit(EXIT_FAILURE);
+            }
+
             exit(EXIT_SUCCESS);
         }
     }
